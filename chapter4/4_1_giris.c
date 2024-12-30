0fc5f0e60c9126846aee78787f4a6c609c42253e
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2024-12-30 11:52:12 +0300

    Create 4_1_giris.c

---
+++ chapter4/4_1_giris.c
@@ -0,0 +1,12 @@
+/*
+ *Bir programın kontrol akışını etkili bir şekilde yönetmek, başarılı bir yazılım geliştirme sürecinin temel taşlarından biridir. 
+ *Kontrol yapıları, bir programın hangi kod bloklarının çalıştırılacağını ve bu blokların ne zaman çalıştırılacağını belirler.
+ *
+ *Bu bölümde, programların akışını kontrol etmek için kullanılan döngü yapıları ve diğer kontrol ifadelerine odaklanacağız.
+ *Özellikle aşağıdaki kavramları inceleyeceğiz:
+ *
+ *Sayaç kontrollü döngüler (counter-controlled loops): Önceden belirlenmiş sayıda işlem yapmak için kullanılır.
+ *Koşul kontrollü döngüler (condition-controlled loops): Bir koşul doğru olduğu sürece çalışır.
+ *Çoklu seçim yapıları (multiple-selection structures): Birden fazla koşul arasında seçim yapmak için kullanılır.
+ *Bu kontrol yapıları, programlamayı daha esnek ve güçlü hale getirir. Karmaşık işlemler basit bir şekilde modellenebilir ve kod okunabilirliği artar.
+ */
