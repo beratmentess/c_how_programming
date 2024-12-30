ffe60977fbbd062fc6335be70b5240da9b161a5c
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2024-12-30 09:57:33 +0300

    Create 6_1_Introduction.c

---
+++ chapter6/6_1_Introduction.c
@@ -0,0 +1,12 @@
+//Mahir Berat Mentes
+
+/*
+ * Bir dizi (array), aynı türdeki birden fazla veriyi saklamak için kullanılan bir veri yapısıdır.
+ * Diziler, bellekte ardışık olarak depolanan elemanlardan oluşur ve her eleman bir indeksle erişilebilir.
+ * Diziler, büyük miktarda veriyi organize etmek ve işlem yapmak için güçlü bir araçtır.
+ *
+ * Dizilerin Kullanım Alanları:
+ * 1-Birden Çok Veriyi Depolama: Aynı türdeki birden fazla değeri bir arada tutar.
+ * 2-Hızlı Erişim: Elemanlara indeksleme kullanarak hızlı erişim sağlar.
+ * 3-Tekrar Eden İşlemler: Bir döngü kullanarak dizi elemanları üzerinde işlem yapmayı kolaylaştırır.
+ */
