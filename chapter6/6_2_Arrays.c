00fe5aafe5e690ee7c3e27a5cb09fd312a1cefc0
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2024-12-30 10:09:44 +0300

    Create 6_2_Arrays.c

---
+++ chapter6/6_2_Arrays.c
@@ -0,0 +1,37 @@
+/*
+ *Bir dizi, sabit bir boyuta sahip veri elemanlarının sıralı bir koleksiyonudur.
+ *Diziler C programlama dilinde aşağıdaki özelliklere sahiptir:
+ *
+ *Tek Veri Türü:
+ *Bir dizi yalnızca bir veri türündeki elemanları saklayabilir (örneğin, int, float, char).
+ *
+ *Sabit Boyut:
+ *Dizi tanımlandığında boyutu sabitlenir ve değiştirilemez.
+ *
+ *İndeksleme:
+ *Dizideki elemanlara 0'dan başlayan bir indeksle erişilir.
+ *
+ *
+ *Diziye Erişim
+ *Dizinin ilk elemanı: dizi[0]
+ *Dizinin ikinci elemanı: dizi[1]
+ *Son eleman: dizi[boyut - 1]
+ */
+
+// Örnek: Dizi Tanımı ve Kullanımı
+
+#include <stdio.h>
+
+int main() {
+    int dizi[5] = {10, 20, 30, 40, 50}; // Bir dizi tanımla
+
+    printf("Birinci eleman: %d\n", dizi[0]); // İlk eleman
+    printf("Ucuncu eleman: %d\n", dizi[2]); // Üçüncü eleman
+    return 0;
+}
+
+/*
+ *CIKTI
+ *Birinci eleman: 10
+ *Ucuncu eleman: 30
+ */
