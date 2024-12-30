a4ea08de732ec31ee15e699b2ae336a72e81873e
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2024-12-30 10:14:09 +0300

    Create 6_3_defining_arrays.c

---
+++ chapter6/6_3_defining_arrays.c
@@ -0,0 +1,21 @@
+/*
+ *Bir dizi, yandaki sözdizimi ile bildirilir >>  tür dizi_adi[boyut];
+ *tür: Dizinin elemanlarının veri türü.
+ *dizi_adi: Dizinin adı.
+ *boyut: Dizi elemanlarının sayısı.
+ */
+
+//Örnek: Dizi Bildirimi
+
+int sayilar[10]; // 10 elemanlı bir tam sayı dizisi
+float oranlar[5]; // 5 elemanlı bir ondalıklı sayı dizisi
+char harfler[26]; // 26 elemanlı bir karakter dizisi
+
+//Diziler bellekte sabit bir boyuta sahip olduğundan, boyut tanımlanırken dikkat edilmelidir.
+
+/*
+ * Dizinin Varsayılan Değerleri
+ * Eğer bir dizi başlatılmazsa:
+  * Yerel değişken: Rastgele (çöp) değerler içerir.
+  * Global değişken: Tüm elemanlar otomatik olarak sıfırlanır.
+ */
