121a807663614accca655de064ace44dc0de8398
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2025-01-03 14:28:38 +0300

    Update and rename 8.1 to 8_1_giris.c

---
+++ chapter8/8_1_giris.c
@@ -0,0 +1,16 @@
+
+/*
+ *Mahir Berat Mentes
+ */
+
+/*
+ *C dilinde yapılar (structures), farklı türdeki verileri bir araya getiren özel veri tipleridir. 
+ *Yapılar, bir nesneye ait ilgili verileri bir arada tutmak için kullanılır ve karmaşık veri modellerinin oluşturulmasını sağlar.
+ */
+
+/*
+ *Yapıların Avantajları
+ *Birden Çok Veri Türü:Aynı yapıda farklı veri türlerini birleştirebilirsiniz.
+ *Organize Veri:İlgili veriler bir grup altında toplanır, bu da kodun okunabilirliğini artırır.
+ *Yeniden Kullanılabilirlik:Yapılar, birden fazla yerde yeniden kullanılabilir.
+ */
