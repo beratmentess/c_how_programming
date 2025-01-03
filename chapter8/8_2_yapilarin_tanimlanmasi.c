35ad62a1981ece11499651c4bc52185195a503bb
Berat Menteş <167982694+beratmentess@users.noreply.github.com>
2025-01-03 14:31:00 +0300

    Create 8_2_yapilarin_tanimlanmasi.c

---
+++ chapter8/8_2_yapilarin_tanimlanmasi.c
@@ -0,0 +1,32 @@
+/*
+ *Mahir Berat Mentes
+ */
+
+//Bir yapı, struct anahtar kelimesi kullanılarak tanımlanır. Bir yapının üyeleri (elemanları), farklı veri türlerinden olabilir.
+
+"""
+  struct yapı_adi {
+    veri_tipi üye1;
+    veri_tipi üye2;
+    ...
+};
+"""
+
+
+  // Ornek: Yapi Tanimi
+
+  """
+  struct Ogrenci {
+    int id;
+    char isim[50];
+    float notOrtalamasi;
+};
+
+"""
+
+  /*
+   *Bu örnekte:
+    *id: Öğrenci kimlik numarasını tutar.
+    *isim: Öğrenci adını tutan bir karakter dizisi.  
+    *notOrtalamasi: Öğrencinin not ortalamasını tutar.
+   */
